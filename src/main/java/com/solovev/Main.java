2ae89dd213bfafa31a92075bbbbf343b3706f2eb
Solovev Anton <solovev.anton93@gmail.com>
2023-04-25 17:20:20 +0300

    Added class to construct classes based on JSON files

--- src/main/java/com/solovev/Main.java
+++ src/main/java/com/solovev/Main.java
@@ -1,7 +1,27 @@
 package com.solovev;
 
+import com.solovev.repository.FromURLtoFile;
+import com.solovev.util.Json2PojoGenerator;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    //TODO remove exceptions from main
+    public static void main(String[] args) throws IOException {
+        //Generate classes from the given Json File
+        //creating file
+        URL urlUsers = new URL("https://jsonplaceholder.typicode.com/users ");
+        FromURLtoFile users = new FromURLtoFile(urlUsers);
+        String fileName = "Users.json";
+        String dirName = "src/main/java/";
+        users.readAndSave(fileName);
+        //generate classes:
+        Json2PojoGenerator generator = new Json2PojoGenerator(fileName,dirName);
+        String className = "Users";
+        String packageName ="com.solovev.model";
+        generator.generate(className,packageName);
+
     }
 }
---
+++ src/main/java/com/solovev/repository/FromURLtoFile.java
@@ -0,0 +1,54 @@
+package com.solovev.repository;
+
+import java.io.BufferedInputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.net.URL;
+
+/**
+ * Class to generate JSON files got from given URL
+ */
+public class FromURLtoFile {
+    private final URL url;
+    private String content;
+
+    public FromURLtoFile(URL url) {
+        this.url = url;
+    }
+
+    /**
+     * Method that takes content from content string
+     * saves it to file in a root with the given name
+     * Use after read method
+     */
+    public void save(String fileName) throws IOException {
+        try(FileWriter fw = new FileWriter(fileName)) {
+            fw.write(content);
+        }
+    }
+
+    /**
+     * Method that reads a content from the given URL and writes it to memory of the object
+     *
+     * @return string contains what was read
+     * @throws IOException Generation Exception
+     */
+    public String read() throws IOException {
+        try (BufferedInputStream inStream = new BufferedInputStream(url.openStream())) {
+            byte[] bytes = inStream.readAllBytes();
+            content = new String(bytes);
+            return content;
+        }
+    }
+
+    /**
+     * Method to read data from given URL and save date to file with the given name
+     * @param fileName - name of the file
+     * @throws IOException
+     */
+    public void readAndSave(String fileName) throws IOException {
+        read();
+        save(fileName);
+    }
+
+}
